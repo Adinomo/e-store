d395e7036b530bc2628b6f1d4525d460c8e84dc8
Adinomo <austinossy20@gmail.com>
2023-03-12 03:08:04 +0100

    added style for active navs

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { HiShoppingBag } from "react-icons/hi";
+import { NavLink } from "react-router-dom";
 
 function Navbar() {
 	return (
-		<nav className="navbar navbar-expand-lg border-bottom">
+		<nav className="navbar navbar-expand-lg border-bottom container">
 			<div className="container-fluid border-1">
 				<a
 					className="navbar-brand text-primary"
@@ -17,21 +18,23 @@ function Navbar() {
 						className="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll"
 						style={{ scrollHeight: "100px" }}>
 						<li className="nav-item">
-							<a
-								className="nav-link"
-								aria-current="page"
-								href="#">
+							<NavLink
+								className="nav-link nav-hover"
+								style={({ isActive }) => ({
+									borderBottom: isActive ? "2px solid hsl(26, 100%, 55%)" : "",
+								})}
+								to="/">
 								Home
-							</a>
+							</NavLink>
 						</li>
 					</ul>
 					<ul className="navbar-nav">
 						<li className="nav-item">
-							<a
-								href="#"
+							<NavLink
+								to="/cart"
 								className="nav-link px-3">
-								<HiShoppingBag size={28}/>
-							</a>
+								<HiShoppingBag size={28} />
+							</NavLink>
 						</li>
 					</ul>
 				</div>
