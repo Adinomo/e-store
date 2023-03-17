73a76f179b13f073d0d283756235eb00fc1fb116
Adinomo <austinossy20@gmail.com>
2023-03-18 00:46:23 +0100

    multi-device responsiveness

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,30 +1,37 @@
 import React from "react";
 import image from "../assets/images/image-product-1.jpg";
-import deleteImg from "../assets/images/icon-close.svg";
+import deleteImg from "../assets/images/icon-delete.svg";
+import { MdDelete } from 'react-icons/md';
+import { IconContext } from "react-icons";
 
 function CartItem({ title, img, price, cartQuantity }) {
 	return (
 		<div className="container-fluid mt-2">
-			<div className="cart-item pb-2 border-bottom d-flex justify-content-between align-items-center">
+			<div className="cart-item pb-2 pt-2 border-bottom d-block d-md-flex align-items-center">
 				<div className="d-flex align-items-center cart-info">
 					<img
 						src={img}
 						alt=""
 						className="cart-item-img"
 					/>
-					<h4 className="px-2">{title}</h4>
+					<h5 className="px-2">{title}</h5>
 				</div>
-				<p>{`$${price * cartQuantity}`}</p>
-				<div className="cart-item-quantity d-flex ">
-					<div className="border p-1 px-3">-</div>
-					<div className="border p-1 px-3">{cartQuantity}</div>
-					<div className="border p-1 px-3">+</div>
+				<p className="mt-3">
+					<span className="d-md-none">Price: </span>
+					<span className="fw-bold">{`$${price * cartQuantity}`}</span>
+				</p>
+				<div className="cart-item-quantity d-flex align-items-center">
+					<span className="d-md-none pe-2">Quantity:</span>
+					<div className="d-flex border rounded-pill">
+						<div className="p-1 px-3 border-end fw-bold">-</div>
+						<div className="p-1 px-3 border-end fw-bold">{cartQuantity}</div>
+						<div className="p-1 px-3 fw-bold">+</div>
+					</div>
 				</div>
-				<img
-					src={deleteImg}
-					alt="delete"
-					className="cart-delete"
-				/>
+				<IconContext.Provider
+					value={{ size: "1.3rem", color: "#949aa5", className: "mt-3 mt-md-0 pointer" }}>
+					<MdDelete />
+				</IconContext.Provider>
 			</div>
 		</div>
 	);
